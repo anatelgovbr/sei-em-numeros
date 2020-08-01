f735ef7b963a7b3e77f78058e344bd9d23c8b499
Nei Jobson <neijobson@anatel.gov.br>
2020-08-01 12:20:38 -0300

    Melhorias no script pós restore

--- SEI_limpexa_dados_pos_restore_d-1.sql
+++ SEI_limpexa_dados_pos_restore_d-1.sql
@@ -1,39 +1,39 @@
--- Limpeza pos restore do SEI
-	truncate prod_sei3.infra_auditoria;
-	truncate prod_sei3.seq_infra_auditoria;
-	truncate prod_sei3.infra_log;
-	truncate prod_sei3.seq_infra_log;
-	truncate prod_sei3.infra_navegador;
-	truncate prod_sei3.seq_infra_navegador;
-
-	update prod_sei3.acesso_externo set hash_interno = 'hash_fake';
-	update prod_sei3.anotacao set descricao = 'Texto Anotação Fake';
-	update prod_sei3.servico set servidor = '*';
-	
-	-- A linha abaixo limpa o conteúdo de documentos, EXCETO dos Publicados no BSE
-	update prod_sei3.documento_conteudo dc
-		left join (select distinct id_documento from prod_sei3.publicacao) as dp using (id_documento)
-    set conteudo_assinatura = null
-		where dc.conteudo_assinatura is not null and dp.id_documento is null;
-	
-	-- Limpeza de conteúdo dos documentos em diversos pontos no banco, inclusive de minutas e versões anteriores do documento
-	update prod_sei3.documento_conteudo set conteudo = null
-		where conteudo is not null;
-	update prod_sei3.documento_conteudo set crc_assinatura = '11111111'
-		where crc_assinatura is not null;
-	update prod_sei3.documento_conteudo set qr_code_assinatura = null
-		where qr_code_assinatura is not null;
-	update prod_sei3.versao_secao_documento set conteudo = '<p>Conte&uacute;do removido.</p>'
-		where conteudo is not null;
-
 -- Limpeza pos restore do SIP
-	truncate prod_sip3.infra_auditoria;
-	truncate prod_sip3.seq_infra_auditoria;
-	truncate prod_sip3.infra_log;
-	truncate prod_sip3.seq_infra_log;
-	truncate prod_sip3.login;
+	TRUNCATE prod_sip3.infra_auditoria;
+	TRUNCATE prod_sip3.seq_infra_auditoria;
+	TRUNCATE prod_sip3.infra_log;
+	TRUNCATE prod_sip3.seq_infra_log;
+	TRUNCATE prod_sip3.login;
 
 -- Ajustes no SIP
-    UPDATE prod_sip3.sistema SET pagina_inicial = "https://seisu.anatel.gov.br/sip" where sigla like "SIP";
-    UPDATE prod_sip3.sistema SET pagina_inicial = "https://seisu.anatel.gov.br/sei/inicializar.php", web_service = "http://seisu.anatel.gov.br/sei/controlador_ws.php?servico=sip" where sigla like "SEI";
+	UPDATE prod_sip3.sistema SET pagina_inicial = 'https://seisu.anatel.gov.br/sip' WHERE sigla = 'SIP';
+	UPDATE prod_sip3.sistema SET pagina_inicial = 'https://seisu.anatel.gov.br/sei/inicializar.php', web_service = 'http://seisu.anatel.gov.br/sei/controlador_ws.php?servico=sip' WHERE sigla = 'SEI';
 	UPDATE prod_sip3.orgao SET sin_autenticar = 'N' WHERE (`id_orgao` = '0');
+	
+	USE prod_sip3;
+	SET FOREIGN_KEY_CHECKS = 1;
+
+-- Limpeza pos restore do SEI
+	TRUNCATE prod_sei3.infra_auditoria;
+	TRUNCATE prod_sei3.seq_infra_auditoria;
+	TRUNCATE prod_sei3.infra_log;
+	TRUNCATE prod_sei3.seq_infra_log;
+	TRUNCATE prod_sei3.infra_navegador;
+	TRUNCATE prod_sei3.seq_infra_navegador;
+
+	UPDATE `prod_sei3`.`infra_parametro` SET `valor` = 'naoresponda_sei_su@anatel.gov.br' WHERE (`nome` = 'SEI_EMAIL_SISTEMA');
+	UPDATE prod_sei3.acesso_externo SET hash_interno = 'hash_fake';
+	UPDATE prod_sei3.anotacao SET descricao = 'Texto Anotação Fake';
+	UPDATE prod_sei3.servico SET servidor = '*';
+	
+	-- Limpeza de conteúdo dos documentos em diversos pontos no banco, inclusive de minutas e versões anteriores do documento
+	UPDATE prod_sei3.documento_conteudo SET conteudo = NULL WHERE conteudo IS NOT NULL;
+	UPDATE prod_sei3.documento_conteudo SET crc_assinatura = '11111111' WHERE crc_assinatura IS NOT NULL;
+	UPDATE prod_sei3.documento_conteudo SET qr_code_assinatura = NULL WHERE qr_code_assinatura IS NOT NULL;
+	UPDATE prod_sei3.versao_secao_documento SET conteudo = '<p>Conteúdo removido.</p>' WHERE conteudo IS NOT NULL;
+	
+	-- Altera a Senha de todos os Usuários Externos para teste123
+	UPDATE prod_sei3.usuario SET senha = '$2a$12$GH/OQlX6.mZaTJfcSo4Fx.53ltB5WJc6Z2k0y2Fy4u0oBs1JOgGZC' WHERE (senha IS NOT NULL);
+		
+	USE prod_sei3;
+	SET FOREIGN_KEY_CHECKS = 1;
